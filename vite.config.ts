84c621057e6f16a217e77b473b04e3ce22d2f788
Vamabam <vincentyounes04@gmail.com>
2024-07-18 16:55:53 +1000

    Rebuilt Amplify API with proper data table

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+  },
+  build: {
+    rollupOptions: {
+      external: ['core-js/modules/web.dom.iterable'],
+    }
   }
 })
