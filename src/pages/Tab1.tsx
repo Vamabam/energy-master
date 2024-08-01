7845d88818b4f39a73ae38b736cefbd3ace4d4d6
Vamabam <vincentyounes04@gmail.com>
2024-08-01 15:16:57 +1000

    Added realtime current and voltage for each device

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,41 +1,45 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { IonContent, IonHeader, IonListHeader, IonPage, IonTitle, IonToolbar, IonCard, IonCardHeader, IonCardSubtitle, IonCardTitle, IonCardContent, IonGrid, IonRow, IonCol } from '@ionic/react';
 import { Chart } from 'chart.js/auto'; // Import from 'chart.js/auto' for the latest version
 import './Tab1.css';
-import useChart from "./useChart";
-import RecentCard from '../components/recentDataCard';
+import { useChart } from "./useChart";
+import {RecentCard, CurrnetCard} from '../components/recentDataCard';
 
 export interface MostRecent {
   recentData: number;
 }
 
+export interface MostRecentCurrVolt {
+  Current: number;
+  Voltage: number;
+}
+
 const Tab1: React.FC = () => {
   // Sets up graph and live data referene for each dataset
-  const [plugPower, setPlugPower] = useState<MostRecent>({ recentData: 10 });
-  const plug1Chart = useRef<HTMLCanvasElement | null>(null);
-  const plug1chartInstance = useRef<Chart<'line'> | null>(null);
-  useChart(plug1Chart, plug1chartInstance, "Plug1", setPlugPower);
-
-  const [solarPower, setSolarPower] = useState<MostRecent>({ recentData: 10 });
+  const [solarCur, setSolarCur] = useState<MostRecentCurrVolt>({ Voltage: 0, Current:0 });
+  const [solarPower, setSolarPower] = useState<MostRecent>({ recentData: 0 });
   const solarChart = useRef<HTMLCanvasElement | null>(null);
   const SolarchartInstance = useRef<Chart<'line'> | null>(null);
-  useChart(solarChart, SolarchartInstance, "CurrentSensor1", setSolarPower);
-
-  const [mainsPower, setMainsPower] = useState<MostRecent>({ recentData: 4 });
+  useChart(solarChart, SolarchartInstance, "CurrentSensor1", setSolarPower, setSolarCur);
+ 
+  const [mainsCur, setMainsCur] = useState<MostRecentCurrVolt>({ Voltage: 0, Current:0 });
+  const [mainsPower, setMainsPower] = useState<MostRecent>({ recentData: 0 });
   const mainsChart = useRef<HTMLCanvasElement | null>(null);
   const mainschartInstance = useRef<Chart<'line'> | null>(null);
-  useChart(mainsChart, mainschartInstance, "CurrentSensor2", setMainsPower);
-
+  useChart(mainsChart, mainschartInstance, "CurrentSensor3", setMainsPower,setMainsCur);
+ 
+  
+  const [circuit1, setCircuit1] = useState<MostRecentCurrVolt>({ Voltage: 0, Current:0 });
   const [circuitPower, setCircuitPower] = useState<MostRecent>({ recentData: 0 });
   const circuitChart = useRef<HTMLCanvasElement | null>(null);
   const circuitchartInstance = useRef<Chart<'line'> | null>(null);
-  useChart(circuitChart, circuitchartInstance, "CurrentSensor3", setCircuitPower);
+  useChart(circuitChart, circuitchartInstance, "CurrentSensor2", setCircuitPower, setCircuit1);
 
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Power Usage</IonTitle>
+          <IonTitle>Energy Master</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
@@ -53,6 +57,7 @@ const Tab1: React.FC = () => {
                 </IonCol>
                 <IonCol size="3">
                   <RecentCard number={solarPower.recentData.toFixed(2)} />
+                  <CurrnetCard voltage={solarCur.Voltage.toFixed(2)} current={solarCur.Current.toFixed(2)} />
                 </IonCol>
               </IonRow>
             </IonGrid>
@@ -71,24 +76,7 @@ const Tab1: React.FC = () => {
                 </IonCol>
                 <IonCol size="3">
                   <RecentCard number={mainsPower.recentData.toFixed(2)} />
-                </IonCol>
-              </IonRow>
-            </IonGrid>
-          </IonCardContent>
-        </IonCard>
-        <IonCard>
-          <IonCardHeader>
-            <IonCardSubtitle>Power Usage (Watts)</IonCardSubtitle>
-            <IonCardTitle>Smart Plug</IonCardTitle>
-          </IonCardHeader>
-          <IonCardContent>
-            <IonGrid>
-              <IonRow>
-                <IonCol size="9">
-                  <canvas ref={plug1Chart}></canvas>
-                </IonCol>
-                <IonCol size="3">
-                  <RecentCard number={plugPower.recentData.toFixed(2)} />
+                  <CurrnetCard voltage={mainsCur.Voltage.toFixed(2)} current={mainsCur.Current.toFixed(2)} />
                 </IonCol>
               </IonRow>
             </IonGrid>
@@ -107,6 +95,7 @@ const Tab1: React.FC = () => {
                 </IonCol>
                 <IonCol size="3">
                   <RecentCard number={circuitPower.recentData.toFixed(2)} />
+                  <CurrnetCard voltage={circuit1.Voltage.toFixed(2)} current={circuit1.Current.toFixed(2)} />
                 </IonCol>
               </IonRow>
             </IonGrid>
